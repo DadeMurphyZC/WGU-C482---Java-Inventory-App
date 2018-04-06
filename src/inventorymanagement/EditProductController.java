17e277de9f022ffcde2818fcd9d99655b51d6375
cfonseca <cfonseca@IT2447.ci.bakersfield.ca.us>
2018-04-06 08:09:58 -0700

    added @FXML gui elements

--- src/inventorymanagement/EditProductController.java
+++ src/inventorymanagement/EditProductController.java
@@ -11,6 +11,10 @@ import javafx.fxml.Initializable;
 import inventorymanagement.Inventory;
 import static inventorymanagement.Inventory.getProducts;
 import static inventorymanagement.MainScreenController.getTempPartIndex;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -18,23 +22,44 @@ import static inventorymanagement.MainScreenController.getTempPartIndex;
  * @author cfonseca
  */
 
-static Product tempProduct;
+
 
 public class EditProductController implements Initializable {
 
+    static Product tempProduct;
+
+@FXML private Button productSearchButton;
+    @FXML private TextField productSearchField;
+    @FXML private Button productPartAddButton;
+    @FXML private Button productPartDeleteButton;
+    @FXML private Button productSaveButton;
+    @FXML private Button productCancelButton;
+    @FXML Label productIDLabel;
+    @FXML Label productNameLabel;
+    @FXML Label productInvLabel;
+    @FXML Label productPriceLabel;
+    @FXML Label productMaxLabel;
+    @FXML Label productMinLabel;
+    @FXML TextField editproductIDField;
+    @FXML TextField editproductNameField;
+    @FXML TextField editproductInvField;
+    @FXML TextField editproductPriceField;
+    @FXML TextField editproductMaxField;
+    @FXML TextField editproductMinField;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         tempProduct = MainScreenController.productData.get(MainScreenController.getTempPartIndex());
-        System.out.println(tempPart.getName() + " " + tempPart.getPartID());
-        editpartId.setText(String.valueOf(tempPart.getPartID()));
-        editpartName.setText(tempPart.getName());
-        editpartInv.setText(Integer.toString(tempPart.getInStock()));
-        editpartPrice.setText(Double.toString(tempPart.getPrice()));
-        editpartMax.setText(Integer.toString(tempPart.getMax()));
-        editpartMin.setText(Integer.toString(tempPart.getMin()));
+        System.out.println(tempProduct.getName() + " " + tempProduct.getProductID());
+        editproductIDField.setText(String.valueOf(tempProduct.getProductID()));
+        editproductNameField.setText(tempProduct.getName());
+        editproductInvField.setText(Integer.toString(tempProduct.getInStock()));
+        editproductPriceField.setText(Double.toString(tempProduct.getPrice()));
+        editproductMaxField.setText(Integer.toString(tempProduct.getMax()));
+        editproductMinField.setText(Integer.toString(tempProduct.getMin()));
     }    
     
 }
