6364dafb355142bc5ed0a82315f49080a602f078
DadeMurphyZC <cfonseca@protonmail.com>
2018-04-06 16:55:33 -0700

    Added Confirm class for custom dialog confirmation

---
+++ src/inventorymanagement/Confirm.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventorymanagement;
+
+import java.util.Optional;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+/**
+ *
+ * @author cfonseca
+ */
+public class Confirm {
+    
+    public static boolean delete(){
+        Boolean choice;
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Please confirm:");
+        alert.setHeaderText(null);
+        alert.setContentText("Are you sure you want to delete this part?");
+        Optional<ButtonType> result = alert.showAndWait();
+        if(result.get() == ButtonType.OK){
+            choice = true;
+        } else {
+            choice = false;
+        }
+        return choice;
+    }
+    
+}
