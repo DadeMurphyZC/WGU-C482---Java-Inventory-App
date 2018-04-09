bee78c1c5592b1e88ffca648e8d699fcdeae120a
DadeMurphyZC <cfonseca@protonmail.com>
2018-04-09 16:39:21 -0700

    format/cleanup

--- src/inventorymanagement/InventoryManagement.java
+++ src/inventorymanagement/InventoryManagement.java
@@ -16,7 +16,7 @@ import javafx.stage.Stage;
  * @author cris
  */
 public class InventoryManagement extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
@@ -31,5 +31,5 @@ public class InventoryManagement extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
