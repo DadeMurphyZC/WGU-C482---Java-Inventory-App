056856f901ae6ad9e0de3591b2a774448471f9ee
DadeMurphyZC <cfonseca@protonmail.com>
2018-04-11 16:03:18 -0700

    Updated fields to private to ensure only getter() setter() are used

--- src/inventorymanagement/Part.java
+++ src/inventorymanagement/Part.java
@@ -11,12 +11,12 @@ package inventorymanagement;
  */
 public class Part {
 
-    int partID;
-    String name;
-    double price;
-    int inStock;
-    int min;
-    int max;
+    private int partID;
+    private String name;
+    private double price;
+    private int inStock;
+    private int min;
+    private int max;
 
     Part(int partID, String name, double price, int inStock, int min, int max) {
         this.partID = partID;
