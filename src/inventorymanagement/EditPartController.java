85bfe0350f15aade4ef8f1fef1726b5f8842133d
DadeMurphyZC <cfonseca@protonmail.com>
2018-05-02 15:58:48 -0700

    editpartInv initialize value was incorrectly set to tempPart.getPrice() set it to tempPart.getInStock()

--- src/inventorymanagement/EditPartController.java
+++ src/inventorymanagement/EditPartController.java
@@ -25,50 +25,63 @@ import javafx.scene.control.ToggleGroup;
  */
 public class EditPartController implements Initializable {
 
-    
-    @FXML private Label mainScreenLabel;
-    @FXML private ToggleGroup edittoggle;
-    @FXML private Button editpartsave;
-    @FXML private Button editpartcancel;
-    @FXML private Label editpartchoicelabel;
-    @FXML private RadioButton editpartinhouse;
-    @FXML private RadioButton editpartoutsourced;
-    @FXML private TextField editpartId;
-    @FXML private TextField editpartName;
-    @FXML private TextField editpartInv;
-    @FXML private TextField editpartPrice;
-    @FXML private TextField editpartMax;
-    @FXML private TextField editpartMin;
-    @FXML private TextField editpartCName; 
-    
+    @FXML
+    private Label mainScreenLabel;
+    @FXML
+    private ToggleGroup edittoggle;
+    @FXML
+    private Button editpartsave;
+    @FXML
+    private Button editpartcancel;
+    @FXML
+    private Label editpartchoicelabel;
+    @FXML
+    private RadioButton editpartinhouse;
+    @FXML
+    private RadioButton editpartoutsourced;
+    @FXML
+    private TextField editpartId;
+    @FXML
+    private TextField editpartName;
+    @FXML
+    private TextField editpartInv;
+    @FXML
+    private TextField editpartPrice;
+    @FXML
+    private TextField editpartMax;
+    @FXML
+    private TextField editpartMin;
+    @FXML
+    private TextField editpartCName;
+
     static Part tempPart;
     public static SceneSwitch editScene = new SceneSwitch();
-    
+
     @FXML
-    private void editpartradioswitch(ActionEvent event){
-        if(editpartinhouse.isSelected()){
+    private void editpartradioswitch(ActionEvent event) {
+        if (editpartinhouse.isSelected()) {
             editpartchoicelabel.setText("Machine ID");
         }
-        if(editpartoutsourced.isSelected()) {
+        if (editpartoutsourced.isSelected()) {
             editpartchoicelabel.setText("Company Name");
         }
     }
-    
+
     @FXML
-    private void editpart(Event event) throws IOException{
-        if(Validator.isminmaxValid(editpartMin, editpartMax)&&
-           Validator.maxInv(editpartInv, editpartMax, editpartMin))
-        {   if(MainScreenController.data.contains(tempPart)) {
-            tempPart = new Part(Integer.parseInt(editpartId.getText()),
-                                editpartName.getText(),
-                                Double.parseDouble(editpartPrice.getText()),
-                                Integer.parseInt(editpartInv.getText()),
-                                Integer.parseInt(editpartMin.getText()),
-                                Integer.parseInt(editpartMax.getText()));
-            MainScreenController.data.set(MainScreenController.getTempPartIndex(), tempPart);
-            editScene.setButton(editpartsave);
-            editScene.setFile("MainScreen.fxml");
-            editScene.sceneSwitch();
+    private void editpart(Event event) throws IOException {
+        if (Validator.isminmaxValid(editpartMin, editpartMax)
+                && Validator.maxInv(editpartInv, editpartMax, editpartMin)) {
+            if (MainScreenController.data.contains(tempPart)) {
+                tempPart = new Part(Integer.parseInt(editpartId.getText()),
+                        editpartName.getText(),
+                        Double.parseDouble(editpartPrice.getText()),
+                        Integer.parseInt(editpartInv.getText()),
+                        Integer.parseInt(editpartMin.getText()),
+                        Integer.parseInt(editpartMax.getText()));
+                MainScreenController.data.set(MainScreenController.getTempPartIndex(), tempPart);
+                editScene.setButton(editpartsave);
+                editScene.setFile("MainScreen.fxml");
+                editScene.sceneSwitch();
             }
         } else {
             Validator.showAlert("\nPlease check the following: "
@@ -76,20 +89,20 @@ public class EditPartController implements Initializable {
                     + "\nInv value must be between MIN and MAX values.");
         }
     }
-    
+
     @FXML
-    private void editpartCancel() throws IOException{
-        if(Confirm.cancel()==true){
+    private void editpartCancel() throws IOException {
+        if (Confirm.cancel() == true) {
             editScene.setButton(editpartcancel);
             editScene.setFile("MainScreen.fxml");
             editScene.sceneSwitch();
         }
     }
-    
+
     @FXML
-    private void deletepart(Event event) throws IOException{
-        if(Confirm.delete()==true){
-            if(MainScreenController.data.contains(tempPart)){
+    private void deletepart(Event event) throws IOException {
+        if (Confirm.delete() == true) {
+            if (MainScreenController.data.contains(tempPart)) {
                 MainScreenController.data.remove(tempPart);
             }
             editScene.setButton(editpartsave);
@@ -97,9 +110,10 @@ public class EditPartController implements Initializable {
             editScene.sceneSwitch();
         }
     }
-    
+
     /**
      * Initializes the controller class.
+     *
      * @param url
      * @param rb
      */
@@ -112,7 +126,7 @@ public class EditPartController implements Initializable {
         editpartInv.setText(Integer.toString(tempPart.getInStock()));
         editpartPrice.setText(Double.toString(tempPart.getPrice()));
         editpartMax.setText(Integer.toString(tempPart.getMax()));
-        editpartMin.setText(Integer.toString(tempPart.getMin()));  
-    }    
-    
+        editpartMin.setText(Integer.toString(tempPart.getMin()));
+    }
+
 }
